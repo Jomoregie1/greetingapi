a6811cf6b490bf2ba868fdce92782bd3711bf924
Joseph <josephomoregie1@outlook.com>
2023-10-13 10:03:21 +0100

    configured the connection to the databse in connection.py

---
+++ app/database/connection.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from decouple import config
+
+# Uses config to retrieve the database url from .env file
+DATABASE_URL = config('DATABASE_URL')
+
+# Creates an engine to manage database connection to excute queries
+engine = create_engine(DATABASE_URL)
+
+# Creates a factory for creating new databse sessions.
+SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+# This creates a new base class for my models, which allows me to define tables and python classes
+base = declarative_base()
