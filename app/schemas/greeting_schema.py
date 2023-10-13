090154c7e320405cc8474ddfd53126785f35c745
Joseph <josephomoregie1@outlook.com>
2023-10-13 13:44:46 +0100

    added pydantic models to greeting_schema.py

---
+++ app/schemas/greeting_schema.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+# This represents the data that defines a greeting
+class GreetingBase(BaseModel):
+    message: str
+    type: str
+
+
+#  This defines my greeting table with the additional fields.
+class Greeting(GreetingBase):
+    greeting_id: int
+    created_at: datetime
+    message_hash: str
+
+    class Config:
+        orm_mode = True
